8440b4bf97bdacea2b6d36441ff3ad5795b0180a
xanderj <xanderj@student.ubc.ca>
2024-05-09 12:34:43 +0200

    Added tsconfig.json and other stuff

---
+++ src/controller/ProductFilter.ts
@@ -0,0 +1,13 @@
+import JSZip from "jszip";
+import * as fs from "fs-extra";
+
+export default class ProductFilter {
+    constructor() {
+
+    }
+
+    public async Loooolaite(): Promise<string> {
+        const zip = new JSZip();
+        return Promise.resolve("yes");
+    }
+}
---
+++ test/controller/ProductFilter.spec.ts
@@ -0,0 +1,10 @@
+import ProductFilter from "../../src/controller/ProductFilter";
+
+
+import chai = require("chai");
+import chaiAsPromised = require("chai-as-promised");
+
+chai.use(chaiAsPromised);
+
+let expect = chai.expect;
+let assert = chai.assert;
